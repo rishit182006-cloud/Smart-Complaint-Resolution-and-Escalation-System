57481f80e0b5f4418d56849c4f30e8be1e764625
rishit182006@gmail.com <rishit182006@gmail.com>
2026-04-10 02:31:59 +0530

    Improve CORS configuration for production

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // DB connect
